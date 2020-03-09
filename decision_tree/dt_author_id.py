ac184249c93a660913d741f6610fde5c301f84f6
AliGhadirii <ali.ghadiri78>
2020-03-09 11:14:56 +0330

    DecisionTree mini project Done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -14,13 +14,19 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print(len(features_train[0]))
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(pred,labels_test))
 
 
 
